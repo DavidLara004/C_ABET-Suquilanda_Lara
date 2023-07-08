c93c3730c1d36722df4c3f289cc8877683d44db9
David Lara <david.lara.moreira@udla.edu.ec>
2023-07-07 21:45:46 -0500

    Forma basica: Escritura en archivo

--- Abetfinal.c
+++ Abetfinal.c
@@ -1,32 +1,47 @@
-#include<stdio.h>
-#include<string.h>
-struct datos{
+#include <stdio.h>
+#include <string.h>
+struct datos
+{
     char nombre[20];
     int cantidad;
     float produccion;
     float venta;
     float ganancia;
 };
-int main(){
+int main()
+{
     struct datos p[2];
     char nom[20];
     int cant;
     float prod, vent, gana[2];
-    for(int i=0; i<2; i++){
+    FILE *archivo, *temporal;
+    for (int i = 0; i < 2; i++)
+    {
         printf("Ingresa el nombre del produccto: ");
-        scanf("%s",&nom);
+        scanf("%s", &nom);
         printf("Ingresa la cantidad de productos: ");
-        scanf("%d",&cant);
+        scanf("%d", &cant);
         printf("Ingresa el precio unitario de produccion: ");
-        scanf("%f",&prod);
+        scanf("%f", &prod);
         printf("Ingresa el precio unitario de venta: ");
-        scanf("%f",&vent);
-        gana[i] = ((vent - prod)*cant);
+        scanf("%f", &vent);
+        gana[i] = ((vent - prod) * cant);
         strcpy(p[i].nombre, nom);
         p[i].cantidad = cant;
         p[i].produccion = prod;
         p[i].venta = vent;
         p[i].ganancia = gana[i];
     }
+    archivo = fopen("Productos,csv", "w");
+    for (int i = 0; i < 2; i++)
+    {
+        fprintf(archivo, "%s;", p[i].nombre);
+        fprintf(archivo, "%d;", p[i].cantidad);
+        fprintf(archivo, "%.2f;", p[i].produccion);
+        fprintf(archivo, "%.2f;", p[i].venta);
+        fprintf(archivo, "%.2f;", p[i].ganancia);
+        fprintf(archivo, "\n");
+    }
+    fclose(archivo);
     return 0;
 }
