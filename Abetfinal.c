207fb31dbe74b03c63eb297578e9f0203c3c69b4
David Lara <david.lara.moreira@udla.edu.ec>
2023-07-08 10:43:55 -0500

    Forma basica: Borrado de archivos por David Lara y Victor Suquilanda

--- Abetfinal.c
+++ Abetfinal.c
@@ -12,19 +12,23 @@ int main()
 {
     struct datos p[2];
     char nom[20], linea[100];
-    int cant, filborr, cont;
+    int cant, filborr, cont=0, cont2=1;
     float prod, vent, gana[2];
     FILE *archivo, *temporal;
     for (int i = 0; i < 2; i++)
     {
         printf("Ingresa el nombre del produccto: ");
         scanf("%s", &nom);
+        fflush(stdin);
         printf("Ingresa la cantidad de productos: ");
         scanf("%d", &cant);
+        fflush(stdin);
         printf("Ingresa el precio unitario de produccion: ");
         scanf("%f", &prod);
+        fflush(stdin);
         printf("Ingresa el precio unitario de venta: ");
         scanf("%f", &vent);
+        fflush(stdin);
         gana[i] = ((vent - prod) * cant);
         strcpy(p[i].nombre, nom);
         p[i].cantidad = cant;
@@ -44,21 +48,42 @@ int main()
     }
     fclose(archivo);
 
-    // Borrado de filas:
+    // Impresion de filas de datos
     archivo = fopen("prod.csv", "r");
-    if(archivo){
+    if (archivo)
+    {
         while (fgets(linea, sizeof(linea), archivo))
         {
             if (linea[strlen(linea) - 1] == '\n')
             {
-                linea[strlen(linea) - 1] == '\0';
+                linea[strlen(linea) - 1] = '\0';
             }
-            printf("%s", linea);
+            printf("%s\n", linea);
             cont++;
         }
+        fclose(archivo);
     }
+    
+
+    // borrado de fila
+    archivo = fopen("prod.csv", "r");
+    temporal = fopen("temporal.csv", "w");
     printf("Ingrese el numero de linea que desea modificar: ");
-    scanf("%d", filborr);
-    fclose(archivo);
+    scanf("%d", &filborr);
+    if (archivo && temporal)
+    {
+        while (fgets(linea, sizeof(linea), archivo))
+        {
+            if (cont2 != filborr)
+            {
+                fprintf(temporal, "%s", linea);
+            }
+            cont2++;
+        }
+        fclose(archivo);
+        fclose(temporal);
+        remove("prod.csv");
+        rename("temporal.csv", "prod.csv");
+    }
     return 0;
 }
